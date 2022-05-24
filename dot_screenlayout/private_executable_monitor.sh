158120dfbb01952ddacd63eb862138534f7e622a
adhiwena <adhiwena@gmail.com>
2022-05-24 22:41:15 +0700

    Add dot_config/i3/config Add dot_config/i3/i3blocks.conf Add dot_config/i3/keybindings Add dot_config/i3/scripts/executable_bandwidth2 Add dot_config/i3/scripts/executable_battery-pinebook-pro Add dot_config/i3/scripts/executable_battery1 Add dot_config/i3/scripts/executable_battery2 Add dot_config/i3/scripts/executable_blur-lock Add dot_config/i3/scripts/executable_cpu_usage Add dot_config/i3/scripts/executable_disk Add dot_config/i3/scripts/executable_i3-switch-if-workspace-empty Add dot_config/i3/scripts/executable_keyboard-switch Add dot_config/i3/scripts/executable_keyhint Add dot_config/i3/scripts/executable_keyhint-2 Add dot_config/i3/scripts/executable_literal_empty_workspace Add dot_config/i3/scripts/executable_memory Add dot_config/i3/scripts/executable_openweather Add dot_config/i3/scripts/executable_openweather-city Add dot_config/i3/scripts/executable_openweather.conf Add dot_config/i3/scripts/executable_power-profiles Add dot_config/i3/scripts/executable_powermenu Add dot_config/i3/scripts/executable_ppd-status Add dot_config/i3/scripts/executable_temperature Add dot_config/i3/scripts/executable_volume Add dot_config/i3/scripts/executable_vpn Add dot_config/nitrogen/bg-saved.cfg Add dot_config/nitrogen/nitrogen.cfg Add dot_screenlayout/private_executable_monitor.sh

---
+++ dot_screenlayout/private_executable_monitor.sh
@@ -0,0 +1,2 @@
+!/bin/sh
+xrandr --output DisplayPort-0 --off --output DisplayPort-1 --off --output DisplayPort-2 --off --output HDMI-A-0 --primary --mode 1920x1080 --rate 144 --dpi 90 --pos 0x0 --rotate normal --output DVI-D-0 --off
